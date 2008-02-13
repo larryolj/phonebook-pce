573c158245450200659fae6cda0f2a0491f6dd5a
cktakahasi <cktakahasi@krau-desk.(none)>
2008-02-13 10:43:23 -0300

    phonebook-client: applying GPL3

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 /*
  *
- *  BlueZ - Bluetooth protocol stack for Linux
+ *  Bluetooth PhoneBook Client Access
  *
- *  Copyright (C) 2006-2008  Claudio Takahasi <claudio.takahasi@indt.org.br>
- *  Copyright (C) 2008-2008  Larry Junior <larry.junior@openbossa.org>
+ *  Copyright (C) 2008  Claudio Takahasi <claudio.takahasi@indt.org.br>
+ *  Copyright (C) 2008  Larry Junior <larry.junior@openbossa.org>
  *
- *
- *  This program is free software; you can redistribute it and/or modify
+ *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
@@ -17,9 +16,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdlib.h>
